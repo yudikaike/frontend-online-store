c41d702db5b207386d87a61c591811c5317a3d80
Kaike Yudi <kaikeyudi@outlook.com>
2022-03-11 07:45:52 -0300

    req05: cria funçoes que cuidam da requisiçao por termos e renderiza produtos na pagina

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
+import { getProductsFromCategoryAndQuery } from './services/api';
+import Products from './components/Products';
+import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: '',
+      searchQuery: '',
+      products: [],
+    };
+    this.handleSearchByQuery = this.handleSearchByQuery.bind(this);
+    this.OnClickSearch = this.OnClickSearch.bind(this);
+  }
+
+  handleSearchByQuery({ target }) {
+    const { value } = target;
+    this.setState({
+      inputValue: value,
+    });
+  }
+
+  async OnClickSearch() {
+    const { inputValue } = this.state;
+    if (inputValue !== '') {
+      const request = await getProductsFromCategoryAndQuery('', inputValue);
+      const products = await request.results;
+      this.setState({
+        products,
+        searchQuery: request.query,
+      });
+    } else {
+      this.setState({
+        searchQuery: '',
+      });
+    }
+  }
+
   render() {
+    const { searchQuery, products } = this.state;
     return (
       <div>
-        <input type="text" />
-        <h1 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1>
+        <input
+          onChange={ this.handleSearchByQuery }
+          data-testid="query-input"
+          type="text"
+        />
+        <button
+          onClick={ this.OnClickSearch }
+          data-testid="query-button"
+          type="button"
+        >
+          Pesquisar
+        </button>
+        <Products
+          searchQuery={ searchQuery }
+          products={ products }
+          renderProducts={ this.renderProducts }
+        />
       </div>
     );
   }
