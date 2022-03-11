b4f8bbabbf1fc9e612712b670cb0053179adc95c
luizpastana <lcsp84@gmail.com>
2022-03-11 11:03:42 -0300

    req-4 Renderiza a lista de categorias com seus devidos inputs

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,12 +2,34 @@ import React from 'react';
 import * as api from '../services/api';
 
 class Filter extends React.Component {
-  render() {
-    // console.log(nomes);
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    };
+  }
 
+  componentDidMount() {
+    this.funcTeste();
+  }
+
+  funcTeste = () => {
+    api.getCategories().then((categorie) => {
+      this.setState({ categories: categorie });
+    });
+  }
+
+  render() {
+    const { categories } = this.state;
+    console.log(categories);
     return (
       <div>
-        {api.getCategories().then((categories) => { categories.map(({ name }) => console.log(name)); })}
+        {categories.map((categorie) => (
+          <label data-testid="category" key={ categorie.id } htmlFor="input-categoty">
+            { categorie.name }
+            <input type="radio" id="input-categoty" />
+          </label>
+        ))}
       </div>
     );
   }
